9f140462688e07b92008508e75f015315299fafc
liaosimin <liaosimin@example.com>
2025-05-01 20:13:35 +0800

    init

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import { createPinia } from 'pinia'
+import router from './router'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import './style.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(createPinia())
+app.use(router)
+app.use(Vant)
+app.mount('#app')
